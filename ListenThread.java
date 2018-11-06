e4a4a6de622e5fcbd2a51551f5ce44fc4bba19a5
Bryan <LJWBryan@gmail.com>
2018-11-06 17:48:38 +0800

    Added wireframe for ListenThread.java and RequestThread.java

--- ListenThread.java
+++ ListenThread.java
@@ -18,18 +18,23 @@ class ListenThread extends Thread {
             byte[] buf = new byte[1024];
             DatagramPacket packet = new DatagramPacket(buf, buf.length);
             try {
-                System.out.println("Waiting for new packet...");
                 socket.receive(packet);
             } catch (IOException e) {
                 e.printStackTrace();
             }
+
+            String address = packet.getAddress().toString();
+            // Add origin address of packet to shared list of known peers
+
             String[] msg = (new String(packet.getData())).trim().split(",");
 
-            if (msg[0].equals("chunks")) {
+            if (msg[0].equals("query")) {
                 // Reply with list of available chunks
             } else if (msg[0].equals("request")) {
                 // Get batch of requested chunks
                 // Start new UploadThread to send chunks to requester
+            } else if (msg[0].equals("list")) {
+                // Store peer's list of chunks together with ip address
             }
         }
     }
