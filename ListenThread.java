22afe8de153fced9654f132bcd1e695de22bcd50
Bryan <LJWBryan@gmail.com>
2018-11-06 21:58:44 +0800

    Completed ListenThread.java

--- ListenThread.java
+++ ListenThread.java
@@ -1,11 +1,24 @@
+import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 class ListenThread extends Thread {
     private DatagramSocket socket;
+    private ConcurrentHashMap chunkMap;
+    private ConcurrentHashMap peerMap;
 
-    public ListenThread() {
+    public ListenThread(ConcurrentHashMap<String, byte[]> chunkMap, ConcurrentHashMap<String, Set<String>> peerMap) {
+        this.chunkMap = chunkMap;
+        this.peerMap = peerMap;
         try {
             socket = new DatagramSocket(8000);
         } catch (SocketException e) {
@@ -13,28 +26,63 @@ class ListenThread extends Thread {
         }
     }
 
+    private void storePeerChunkList(String peer, String data) {
+        Set<String> peerChunkSet = new HashSet<String>(Arrays.asList(data.split(",")));
+        if (peerMap.containsKey(address)) {
+            peerMap.replace(address, peerChunkSet);
+        } else {
+            peerMap.put(address, peerChunkSet);
+        }
+    }
+
     public void run() {
+        String peer;
+        String[] data;
+        DatagramPacket packet;
+
         while (true) {
             byte[] buf = new byte[1024];
-            DatagramPacket packet = new DatagramPacket(buf, buf.length);
+            packet = new DatagramPacket(buf, buf.length);
             try {
                 socket.receive(packet);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            String address = packet.getAddress().toString();
-            // Add origin address of packet to shared list of known peers
+            peer = packet.getAddress().toString();
+            data = (new String(packet.getData())).trim().split(",", 2);
 
-            String[] msg = (new String(packet.getData())).trim().split(",");
-
-            if (msg[0].equals("query")) {
+            switch (data[0]) {
+            case "query":
+                // Store peer's list of chunks
+                storePeerChunkList(peer, data[1]);
                 // Reply with list of available chunks
-            } else if (msg[0].equals("request")) {
+                String reply = "list," + chunkMap.keySet().toString().replaceAll("\\[|\\]", "");
+                replyBytes = reply.getBytes();
+                try {
+                    InetAddress address = InetAddress.getByName(peer);
+                    packet = new DatagramPacket(replyBytes, replyBytes.length, address, 8000);
+                } catch (IOException | UnknownHostException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "request":
                 // Get batch of requested chunks
+                List chunks = new ArrayList<byte[]>();
+                for (String chunkNum : data[1].split(",")) {
+                    chunk = chunkMap.get(chunkNum);
+                    chunks.add(chunk);
+                }
                 // Start new UploadThread to send chunks to requester
-            } else if (msg[0].equals("list")) {
-                // Store peer's list of chunks together with ip address
+                Thread uploadThread = new UploadThread(peer, chunks);
+                uploadThread.start();
+                break;
+            case "list":
+                // Store peer's list of chunks
+                storePeerChunkList(peer, data[1]);
+                break;
+            default:
+                break;
             }
         }
     }
