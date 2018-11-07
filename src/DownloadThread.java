a5cc15a6e888b9df9d760547d7da34ede9e4d2a3
BryanLeong <Bryan_Leong@mymail.sutd.edu.sg>
2018-11-07 16:22:01 +0800

    Completed DownloadThread

--- src/DownloadThread.java
+++ src/DownloadThread.java
@@ -2,17 +2,25 @@ import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
-import java.nio.ByteBuffer;
-import java.util.Arrays;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.CopyOnWriteArrayList;
 
 class DownloadThread extends Thread {
     private DatagramSocket socket;
     private ConcurrentHashMap<String, byte[]> chunkMap;
-    private int chunkSize = 1024;
+    private ConcurrentHashMap<String, Set<String>> batchMap;
+    private CopyOnWriteArrayList<String> requestedChunks;
+    private int chunkSize;
 
-    public DownloadThread(ConcurrentHashMap<String, byte[]> chunkMap) {
+    public DownloadThread(int chunkSize,
+                          ConcurrentHashMap<String, byte[]> chunkMap,
+                          ConcurrentHashMap<String, Set<String>> batchMap,
+                          CopyOnWriteArrayList<String> requestedChunks) {
+        this.chunkSize = chunkSize;
         this.chunkMap = chunkMap;
+        this.batchMap = batchMap;
+        this.requestedChunks = requestedChunks;
         try {
             socket = new DatagramSocket(8001);
         } catch (SocketException e) {
@@ -21,19 +29,25 @@ class DownloadThread extends Thread {
     }
 
     public void run() {
+        String peer;
+        String[] data;
+        DatagramPacket packet;
+
         while (true) {
             byte[] buf = new byte[chunkSize];
-            DatagramPacket packet = new DatagramPacket(buf, chunkSize);
+            packet = new DatagramPacket(buf, buf.length);
             try {
                 socket.receive(packet);
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            byte[] chunk = packet.getData();
-            int chunkNum = ByteBuffer.wrap(chunk).getInt();
-            chunk = Arrays.copyOfRange(chunk, 4, chunk.length);
-            chunkMap.putIfAbsent(String.valueOf(chunkNum), chunk);
+            peer = packet.getAddress().getHostAddress();
+            data = (new String(packet.getData())).trim().split(",", 2);
+
+            chunkMap.putIfAbsent(data[0], data[1].getBytes());
+            batchMap.get(peer).remove(data[0]);
+            requestedChunks.remove(data[0]);
         }
     }
 }
