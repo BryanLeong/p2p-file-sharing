f109bcd2f746b1889a5691ad828e757cf46ef753
BryanLeong <Bryan_Leong@mymail.sutd.edu.sg>
2018-11-11 16:53:54 +0800

    Completed FileIOThread.java

--- src/FileIOThread.java
+++ src/FileIOThread.java
@@ -1,18 +1,16 @@
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
+import java.util.stream.Collectors;
 
 class FileIOThread extends Thread {
     private ConcurrentHashMap<String, byte[]> chunkMap;
     private CopyOnWriteArrayList<String> newChunks;
     private File folder;
+    private List<String> fileNameList;
     private List<File> fileList;
     private int chunkSize;
     private Thread updateThread;
@@ -27,6 +25,7 @@ class FileIOThread extends Thread {
         this.chunkMap = chunkMap;
         this.newChunks = newChunks;
         folder = new File("files");
+        fileNameList = Arrays.asList(folder.list());
         fileList = Arrays.asList(folder.listFiles());
         populateChunkMap();
         cdl.countDown();
@@ -36,26 +35,45 @@ class FileIOThread extends Thread {
         Set<String> newChunks = new HashSet<>();
         for (File file : fileList) {
             String fileName = file.getName();
-            try {
-                byte[] fileBytes = Files.readAllBytes(file.toPath());
-                int totalChunks = (int) Math.ceil(1.0 * fileBytes.length / chunkSize);
-                int start = 0, end;
-                for (int i = 1; i <= totalChunks; i++) {
-                    if (start + chunkSize < fileBytes.length) {
-                        end = start + chunkSize;
-                    } else {
-                        end = fileBytes.length;
+            if (fileName.matches("(\\.tmp)$")) {
+                try {
+                    FileInputStream fis = new FileInputStream(file);
+                    ObjectInputStream ois = new ObjectInputStream(fis);
+                    Map<String, byte[]> fileChunks = (ConcurrentHashMap<String, byte[]>) ois.readObject();
+                    ois.close();
+                    fis.close();
+                    for (Map.Entry<String, byte[]> entry : fileChunks.entrySet()) {
+                        String chunkId = entry.getKey();
+                        if (!chunkMap.containsKey(chunkId)) {
+                            chunkMap.put(chunkId, entry.getValue());
+                            newChunks.add(chunkId);
+                        }
                     }
-                    String chunkId = String.format("%s/%d/%d", fileName, totalChunks, i);
-                    byte[] chunk = Arrays.copyOfRange(fileBytes, start, end);
-                    if (!chunkMap.containsKey(chunkId)) {
-                        chunkMap.put(chunkId, chunk);
-                        newChunks.add(chunkId);
-                    }
-                    start += chunkSize;
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
                 }
-            } catch (IOException e) {
-                e.printStackTrace();
+            } else {
+                try {
+                    byte[] fileBytes = Files.readAllBytes(file.toPath());
+                    int totalChunks = (int) Math.ceil(1.0 * fileBytes.length / chunkSize);
+                    int start = 0, end;
+                    for (int i = 1; i <= totalChunks; i++) {
+                        if (start + chunkSize < fileBytes.length) {
+                            end = start + chunkSize;
+                        } else {
+                            end = fileBytes.length;
+                        }
+                        String chunkId = String.format("%s/%d/%d", fileName, totalChunks, i);
+                        byte[] chunk = Arrays.copyOfRange(fileBytes, start, end);
+                        if (!chunkMap.containsKey(chunkId)) {
+                            chunkMap.put(chunkId, chunk);
+                            newChunks.add(chunkId);
+                        }
+                        start += chunkSize;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             }
         }
         return newChunks;
@@ -66,6 +84,7 @@ class FileIOThread extends Thread {
             // update chunkList if new files are added
             List<File> newFileList = Arrays.asList(folder.listFiles());
             if (!fileList.containsAll(newFileList)) {
+                fileNameList = Arrays.asList(folder.list());
                 fileList = newFileList;
                 Set<String> result = populateChunkMap();
                 synchronized (newChunks) {
@@ -77,7 +96,46 @@ class FileIOThread extends Thread {
             }
 
             // if we have all chunks of a file, write it to disk
-            // else store chunks in a temp file on disk (update populateChunkMap() to read these as chunks as well)
+            // else store chunks in a temp file on disk
+            List<String> keyList = new ArrayList<>(chunkMap.keySet());
+            keyList.replaceAll(s -> s.split("/\\d+$")[0]);
+            for (String s : new HashSet<>(keyList)) {
+                String[] fnc = s.split("/");
+
+                if (!fileNameList.contains(fnc[0])) {
+                    Map<String, byte[]> fileChunks = chunkMap.entrySet().stream()
+                            .filter(entry -> entry.getKey().contains(fnc[0]))
+                            .collect(Collectors.toMap(entry -> entry.getKey(), entry -> entry.getValue()));
+
+                    int numChunks = Integer.valueOf(fnc[1]);
+                    String fp = "files/" + fnc[0];
+                    if (fileChunks.size() == numChunks) {
+                        try {
+                            if (fileNameList.contains(fnc[0] + ".tmp")) {
+                                File tmpFile = new File(fp + ".tmp");
+                                tmpFile.delete();
+                            }
+                            FileOutputStream fos = new FileOutputStream(fp);
+                            for (int i = 1; i <= numChunks; i++) {
+                                fos.write(fileChunks.get(s + "/" + i));
+                            }
+                            fos.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    } else {
+                        try {
+                            FileOutputStream fos = new FileOutputStream(fp + ".tmp");
+                            ObjectOutputStream oos = new ObjectOutputStream(fos);
+                            oos.writeObject(fileChunks);
+                            oos.close();
+                            fos.close();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
 
             try {
                 Thread.sleep(5000);
